d7c45f50bf000e0e9733b019563fafa77b483532
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-05-05 17:31:50 +0800

    nacree page ya mbere

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
-
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <header></header>
+    <h2>Memories Page</h2>
+</template>
+<script>
+export default {
+    
+}
+</script>
