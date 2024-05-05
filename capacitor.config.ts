f912bbc59d05d823046f26334a79505eb5b158dc
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-05-05 16:56:41 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'royal_burundi_cli',
+  webDir: 'dist'
+};
+
+export default config;
