c82567802874dd1b20dff54cb64810156ded53e7
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-05-05 17:51:30 +0800

    gutangura gukora login

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,9 +1,31 @@
 <template>
-    <header></header>
-    <h2>Memories Page</h2>
+  <IonPage>
+    <IonHeader>
+        <IonToolbar>
+            <IonTitle>All Memories</IonTitle>
+        </IonToolbar>
+    </IonHeader>
+    <IonContent>
+        <IonList>
+            <IonItem>Max</IonItem>
+            <IonItem>Imbecile</IonItem>
+            <IonItem>Max</IonItem>
+            <IonItem>Max</IonItem>
+        </IonList>
+    </IonContent>
+  </IonPage>
 </template>
 <script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem} from '@ionic/vue'
 export default {
-    
+   components:{
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+    IonList,
+    IonItem
+   } 
 }
 </script>
