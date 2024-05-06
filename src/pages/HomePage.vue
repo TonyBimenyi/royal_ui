191289761ccf6a04918d3abfbc17e12428ccc80a
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-05-06 23:44:04 +0800

    list promo

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,7 @@
 <template>
     <ion-content class="home_content" ion-pading>
-        <ion-list>
             <ion-item>
-                <ion-badge slot="end">22</ion-badge>
+                <ion-badge class="button_order" slot="end">as</ion-badge>
                 <ion-label>
                     <div class="user">
                         <div class="img">
@@ -21,7 +20,7 @@
                         <ion-row>
                         <ion-col class="col_txt">
                             <p>Your Points</p>
-                            <h3>1500.00</h3>
+                            <h3>0.00</h3>
                         </ion-col>
                         <ion-col></ion-col>
                         <ion-col class="qr_code"><i class="fa fa-qrcode" aria-hidden="true"></i></ion-col>
@@ -30,15 +29,17 @@
                     </ion-grid>
                     <ion-grid>
                         <ion-row>
-                        <ion-col class="">
+                        <ion-col class="point_txt">
                             <i class="fa fa-outdent" aria-hidden="true"></i>
                             <p>Send Points</p>
                         </ion-col>
-                        <ion-col class="">
-                            <i class="fa fa-qrcode" aria-hidden="true"></i>
+                        <ion-col class="point_txt">
+                            <i class="fa fa-indent" aria-hidden="true"></i>
+                            <p>Receive Points</p>
                         </ion-col>
-                        <ion-col class="">
-                            <i class="fa fa-qrcode" aria-hidden="true"></i>
+                        <ion-col class="point_txt">
+                            <i class="fa fa-gift" aria-hidden="true"></i>
+                            <p>Rewards</p>
                         </ion-col>
                     
 
@@ -46,7 +47,103 @@
                     </ion-grid>
                 </ion-card-content>
             </ion-card>
+   
+
+        <h2>Special Promotions</h2>
+        
+        <ion-list>
+            <ion-item>
+                <ion-badge class="button_order" slot="end">Order</ion-badge>
+                <ion-label>
+                    <div class="list">
+                        <div class="img_list">
+                            <img src="../theme/images/login_bg.png" alt="">
+                        </div>
+                        <div class="promo">
+                            <p class="username_txt">Limited Time Offer</p>
+                            <p class="welcome_txt">Buy 3 Beers and get 1500 points!</p>
+                            
+                        </div>
+                    </div>
+                </ion-label>
+            </ion-item>
+
+            <ion-item>
+                <ion-badge class="button_order" slot="end">Order</ion-badge>
+                <ion-label>
+                    <div class="list">
+                        <div class="img_list">
+                            <img src="../theme/images/login_bg.png" alt="">
+                        </div>
+                        <div class="promo">
+                            <p class="username_txt">Limited Time Offer</p>
+                            <p class="welcome_txt">Buy 3 Beers and get 1500 points!</p>
+                            
+                        </div>
+                    </div>
+                </ion-label>
+            </ion-item>
+
+            <ion-item>
+                <ion-badge class="button_order" slot="end">Order</ion-badge>
+                <ion-label>
+                    <div class="list">
+                        <div class="img_list">
+                            <img src="../theme/images/login_bg.png" alt="">
+                        </div>
+                        <div class="promo">
+                            <p class="username_txt">Limited Time Offer</p>
+                            <p class="welcome_txt">Buy 3 Beers and get 1500 points!</p>
+                            
+                        </div>
+                    </div>
+                </ion-label>
+            </ion-item>
+
+            <ion-item>
+                <ion-badge class="button_order" slot="end">Order</ion-badge>
+                <ion-label>
+                    <div class="list">
+                        <div class="img_list">
+                            <img src="../theme/images/login_bg.png" alt="">
+                        </div>
+                        <div class="promo">
+                            <p class="username_txt">Limited Time Offer</p>
+                            <p class="welcome_txt">Buy 3 Beers and get 1500 points!</p>
+                            
+                        </div>
+                    </div>
+                </ion-label>
+            </ion-item>
+
+            <ion-item>
+                <ion-badge class="button_order" slot="end">Order</ion-badge>
+                <ion-label>
+                    <div class="list">
+                        <div class="img_list">
+                            <img src="../theme/images/login_bg.png" alt="">
+                        </div>
+                        <div class="promo">
+                            <p class="username_txt">Limited Time Offer</p>
+                            <p class="welcome_txt">Buy 3 Beers and get 1500 points!</p>
+                            
+                        </div>
+                    </div>
+                </ion-label>
+            </ion-item>
+            
         </ion-list>
+        
+        <ion-grid>
+            <ion-row>
+            <ion-col class="col_txt">
+                <p>Explore the bars</p>
+            </ion-col>
+            <ion-col></ion-col>
+            <ion-col class="qr_code"><i class="fa fa-qrcode" aria-hidden="true"></i></ion-col>
+
+            </ion-row>
+        </ion-grid>
     </ion-content>
 </template>
 <script>
@@ -67,7 +164,7 @@ export default {
         IonGrid,
         IonRow,
         IonCol,
-        IonCardContent
+        IonCardContent,
     }
 }
 </script>
