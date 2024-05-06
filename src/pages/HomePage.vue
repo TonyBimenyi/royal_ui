1a7a1a85245f301aa6f3c70992d78cba9203571e
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-05-07 00:52:22 +0800

    home iraheze

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
-    <ion-content class="home_content" ion-pading>
+    <ion-content class="home_content" >
             <ion-item>
-                <ion-badge class="button_order" slot="end">as</ion-badge>
+                <ion-badge class="button_order" slot="end"><i class="fa fa-sign-out" aria-hidden="true"></i></ion-badge>
                 <ion-label>
                     <div class="user">
                         <div class="img">
@@ -47,11 +47,31 @@
                     </ion-grid>
                 </ion-card-content>
             </ion-card>
-   
-
-        <h2>Special Promotions</h2>
+            
+        <h3 class="title_princi"><span><i class="fa fa-caret-right" aria-hidden="true"></i></span> Requested Ads</h3>
         
-        <ion-list>
+
+        <div class="slides" :style="{transform: `translateX(-${currentIndex * 100}%)`}">
+            <!-- Loop through images -->
+            <div class="slide" v-for="(image, index) in images" :key="index">
+              <div class="card">
+                <img :src="image.src" :alt="image.alt">
+              </div>
+            </div>
+          </div>
+          <!-- Navigation points -->
+        <div class="navigation">
+            <span
+              class="navigation-point"
+              v-for="(image, index) in images"
+              :key="index"
+              :class="{ 'active': currentIndex === index }"
+              @click="goToSlide(index)"
+            ></span>
+          </div>
+        <h3 class="title_princi"> <span><i class="fa fa-caret-right" aria-hidden="true"></i></span>Special Promotions</h3>
+        
+        <ion-list class="list_pro">
             <ion-item>
                 <ion-badge class="button_order" slot="end">Order</ion-badge>
                 <ion-label>
@@ -148,6 +168,10 @@
 </template>
 <script>
 import { IonPage,  IonContent, IonCard, IonCardHeader, IonList, IonItem, IonInput, IonIcon,IonButton,IonLabel,IonBadge,IonGrid,IonRow,IonCol,IonCardContent} from "@ionic/vue"
+import myImage1 from '../theme/images/img1.jpg';
+import myImage2 from '../theme/images/img2.jpg';
+import myImage3 from '../theme/images/img3.jpg';
+import myImage4 from '../theme/images/img4.jpg';
 export default {
     components:{
         IonPage,
@@ -165,7 +189,33 @@ export default {
         IonRow,
         IonCol,
         IonCardContent,
-    }
+    },
+    data() {
+        return{
+        currentIndex: 0,
+        images: [
+          { src: myImage1, title: 'Slide 1 Title', content: 'Slide 1 Content' },
+          { src: myImage2, alt: 'Slide 2', title: 'Slide 2 Title', content: 'Slide 2 Content' },
+          { src: myImage3, alt: 'Slide 2', title: 'Slide 3 Title', content: 'Slide 2 Content' },
+          { src: myImage4, alt: 'Slide 2', title: 'Slide 4 Title', content: 'Slide 2 Content' },
+          // Add more images as needed
+        ]
+        }
+      },
+      mounted() {
+        this.startSlider();
+      },
+      methods: {
+        startSlider() {
+          setInterval(() => {
+            this.currentIndex = (this.currentIndex + 1) % this.images.length;
+          }, 3000); // Change slide every 3 seconds (adjust as needed)
+        },
+        goToSlide(index) {
+          this.currentIndex = index;
+        }
+      }
+      
 }
 </script>
 <style src="../theme/home.css" scoped>
